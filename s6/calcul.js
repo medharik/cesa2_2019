7e68f5902ce6afe3c9cdee9a403a7454a32474b0
unknown <med.harik@gmail.com>
2019-10-23 16:23:18 +0000

    JS / bootstrap

---
+++ s6/calcul.js
@@ -0,0 +1,25 @@
+function valider() {
+  var ti = document.getElementsByTagName("input");
+  var al = document.querySelector(".alert-danger");
+  erreur = false;
+
+  for (i = 0; i < ti.length; i++) {
+    if (ti[i].value == "") {
+      erreur = true;
+      // alert(ti[i].name + " champ requis");
+    }
+  }
+
+  if (erreur == true) {
+    al.innerHTML = "tous les champs sont requis ";
+    al.className = "alert alert-danger d-block text-center";
+  }
+}
+
+function changer() {
+  c = document.querySelector("input[type=checkbox]");
+  p = document.getElementById("p");
+  eye = document.getElementById("eye");
+  p.type = c.checked ? "text" : "password";
+  eye.src = c.checked ? "images/open.png" : "images/close.png";
+}
